74fce49583e0a4d7b9c220a712e7e3cbe3edc120
FranciscoDRG <pancho221299@gmail.com>
2022-10-17 11:24:16 -0500

    Borrando api de la rama cliente

--- API/src/controllers/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './ping.controller';
-export * from './cliente.controller';
-export * from './biblioteca.controller';
-export * from './lista-rep.controller';
-export * from './user.controller';
--- API/src/repositories/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './biblioteca.repository';
-export * from './cliente.repository';
-export * from './lista-reproduccion.repository';
