c2d456f4368fbe71321d4824da1815f2ef3d894b
Cesar-CCS <CesarCastro_S@outlook.com>
2022-10-18 09:46:03 -0500

    web

--- Index.js
+++ Index.js
@@ -2,15 +2,13 @@ const Validando = () => {
     console.log(window.localStorage)
     if (window.localStorage.length > 0 ){
         setTimeout(() => {
-            console.log("Entra verdadero");
             location.assign("cliente.html");
-        },200000000000000);
+        },2000);
     } else {
         console.log("falso")
         setTimeout(() => {
-            console.log("Entra Falso")
             location.assign("Login.html");
-        },2000000000000000);
+        },2000);
     }
 }
 
